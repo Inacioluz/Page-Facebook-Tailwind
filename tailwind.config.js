d85edc533c6ce93a345e056172b8b874d7ea26a4
Inacio Silva <joseinaciolds@gmail.com>
2023-08-01 16:41:07 -0300

    commit da pagina sem os conponentes

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'blue-facebook': '#1877F2',
+        'gray-facebook' : '#F0f2f5'
+      },
+      spacing: {
+        '580': '580px',
+        '380': '380px',
+      },
+    },
+  },
+  plugins: [],
+}
+
